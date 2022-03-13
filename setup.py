d42c5866514b2fd38b551e42d773c779a3b51a54
laptop <eric.volz02@gmail.com>
2022-03-13 16:26:54 +0100

    init library

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Defichain Node and Ocean Library'
+
+# Setting up
+setup(
+    name="defichain",
+    version=VERSION,
+    author="Intr0c (Eric Volzp)",
+    author_email="introc@volz.link",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'defichain', 'node', 'ocean'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
