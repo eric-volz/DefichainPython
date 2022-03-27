bff3e6000fb8e672366df0a0ded33ef3c8144e97
laptop <eric.volz02@gmail.com>
2022-03-27 22:34:51 +0200

    finished all calls for Accounts without Docs

--- defichain/src/node/modules/accounts.py
+++ defichain/src/node/modules/accounts.py
@@ -39,7 +39,7 @@ class Accounts:
         j.append("txtype", txtype)
         return self.node.rpc.call("accounthistorycount", owner, j.build())
 
-    def accounttoaccount(self, _from, to, inputs=None):
+    def accounttoaccount(self, _from, to, inputs=None):  # 02
         """
         Creates (and submits to local node and network) a transfer transaction from the specified account to the
         specfied accounts.The first optional argument (may be empty array) is an array of specific UTXOs to spend.
@@ -67,7 +67,7 @@ class Accounts:
         """
         return self.node.rpc.call("accounttoaccount", _from, to, inputs)
 
-    def accounttoutxos(self, _from, to, inputs=None):
+    def accounttoutxos(self, _from, to, inputs=None):  # 03
         """
         Creates (and submits to local node and network) a transfer transaction from the specified account to UTXOs.
         The third optional argument (may be empty array) is an array of specific UTXOs to spend.
@@ -94,14 +94,73 @@ class Accounts:
         """
         return self.node.rpc.call("accounttoutxos", _from, to, inputs)
 
-    def executesmartcontract(self, name, amount, address=None, inputs=None):
+    def executesmartcontract(self, name, amount, address=None, inputs=None):  # 04
         return self.node.rpc.call("executesmartcontract", name, amount, address, inputs)
 
-    def getaccount(self, owner, start=None, including_start=None, limit=None, indexed_amounts=None):
+    def getaccount(self, owner, start=None, including_start=None, limit=None, indexed_amounts=None):  # 05
         pagination = BuildJson()
         pagination.append("start", start)
         pagination.append("including_start", including_start)
         pagination.append("limit", limit)
-        pagination.append("indexed_amounts", indexed_amounts)
 
-        return self.node.rpc.call("getaccount", owner, pagination.build())
+        return self.node.rpc.call("getaccount", owner, pagination.build(), indexed_amounts)
+
+    def getaccounthistory(self, owner, blockheight, txn):  # 06
+        return self.node.rpc.call("getaccounthistory", owner, blockheight, txn)
+
+    def getburninfo(self):  # 07
+        return self.node.rpc.call("getburninfo")
+
+    def gettokenbalances(self, start=None, including_start=None, limit=None, indexed_amounts=None, symbol_lookup=None):  # 08
+        pagination = BuildJson()
+        pagination.append("start", start)
+        pagination.append("including_start", including_start)
+        pagination.append("limit", limit)
+
+        indexed_amounts = False if indexed_amounts is None else indexed_amounts
+
+        return self.node.rpc.call("gettokenbalances", pagination.build(), indexed_amounts, symbol_lookup)
+
+    def listaccounthistory(self, owner, maxBlockHeight=None, depth=None, no_rewards=None, token=None, txtype=None, limit=None):  # 09
+        options = BuildJson()
+        options.append("maxBlockHeight", maxBlockHeight)
+        options.append("depth", depth)
+        options.append("no_rewards", no_rewards)
+        options.append("token", token)
+        options.append("txtype", txtype)
+        options.append("limit", limit)
+
+        return self.node.rpc.call("listaccounthistory", owner, options.build())
+
+    def listaccounts(self, start=None, including_start=None, limit=None, verbose=None, indexed_amounts=None, is_mine_only=None):  # 10
+        pagnation = BuildJson()
+        pagnation.append("start", start)
+        pagnation.append("including_start", including_start)
+        pagnation.append("limit", limit)
+
+        verbose = True if verbose is None else verbose
+        indexed_amounts = False if indexed_amounts is None else indexed_amounts
+
+        return self.node.rpc.call("listaccounts", pagnation.build(), verbose, indexed_amounts, is_mine_only)
+
+    def listburnhistory(self, maxBlockHeight=None, depth=None, token=None, txtype=None, limit=None):  # 11
+        options = BuildJson()
+        options.append("maxBlockHeight", maxBlockHeight)
+        options.append("depth", depth)
+        options.append("token", token)
+        options.append("txtype", txtype)
+        options.append("limit", limit)
+
+        return self.node.rpc.call("listburnhistory", options.build())
+
+    def listcommunitybalances(self):  # 12
+        return self.node.rpc.call("listcommunitybalances")
+
+    def sendtokenstoaddress(self, _from, to, selectionMode=None):  # 13
+        return self.node.rpc.call("sendtokenstoaddress", _from, to, selectionMode)
+
+    def sendutxosfrom(self, _from, to, amount, change=None):  # 14
+        return self.node.rpc.call("sendutxosfrom", _from, to, amount, change)
+
+    def utxostoaccount(self, amounts, inputs=None):  # 15
+        return self.node.rpc.call("utxostoaccount", amounts, inputs)
