14c99d1b0eb189172aa1fd7f44073177259ae547
laptop <eric.volz02@gmail.com>
2022-03-29 00:50:58 +0200

    add new commands to blockchain

--- defichain/src/node/modules/blockchain.py
+++ defichain/src/node/modules/blockchain.py
@@ -9,7 +9,6 @@ class Blockchain:
         return self.node.rpc.call("getbestblockhash")
 
     def getblock(self, blockhash, verbosity=None):  # 03
-        verbosity = verbosity if verbosity is not None else 1
         return self.node.rpc.call("getblock", blockhash, verbosity)
 
     def getblockchaininfo(self):  # 04
@@ -19,7 +18,6 @@ class Blockchain:
         return self.node.rpc.call("getblockcount")
 
     def getblockfilter(self, blockhash, filtertype=None):  # 06
-        filtertype = "basic" if filtertype is None else filtertype
         return self.node.rpc.call("getblockfilter", blockhash, filtertype)
 
     def getblockhash(self, height):  # 07
@@ -28,3 +26,73 @@ class Blockchain:
     def getblockheader(self, blockhash, verbose=None):  # 08
         verbose = True if verbose is None else verbose
         return self.node.rpc.call("getblockheader", blockhash, verbose)
+
+    def getblockstats(self, hash_or_height, stats=None):  # 09
+        return self.node.rpc.call("getblockstats", hash_or_height, stats)
+
+    def getchaintips(self):  # 10
+        return self.node.rpc.call("getchaintips")
+
+    def getchaintxstats(self, nblocks=None, blockhash=None):  # 11
+        nblocks = 86400 if nblocks is None else nblocks
+        return self.node.rpc.call("getchaintxstats", nblocks, blockhash)
+
+    def getdifficulty(self):  # 12
+        return self.node.rpc.call("getdifficulty")
+
+    def getgov(self, name):  # 13
+        return self.node.rpc.call("getgov", name)
+
+    def getmempoolancestors(self, txid, verbose=None):  # 14
+        return self.node.rpc.call("getmempoolancestors", txid, verbose)
+
+    def getmempooldescendants(self, txid, verbose=None):  # 15
+        return self.node.rpc.call("getmempooldescendants", txid, verbose)
+
+    def getmempoolentry(self, txid):  # 16
+        return self.node.rpc.call("getmempoolentry", txid)
+
+    def getmempoolinfo(self):  # 17
+        return self.node.rpc.call("getmempoolinfo")
+
+    def getrawmempool(self, verbose=None):  # 18
+        return self.node.rpc.call("getrawmempool", verbose)
+
+    def gettxout(self):
+        pass
+
+    def gettxoutproof(self):
+        pass
+
+    def gettxoutsetinfo(self):
+        pass
+
+    def isappliedcustomtx(self):
+        pass
+
+    def listgovs(self):
+        pass
+
+    def preciousblock(self):
+        pass
+
+    def pruneblockchain(self):
+        pass
+
+    def savemempool(self):
+        pass
+
+    def scantxoutset(self):
+        pass
+
+    def setgov(self):
+        pass
+
+    def setgovheight(self):
+        pass
+
+    def verifychain(self):
+        pass
+
+    def verifytxoutproof(self):
+        pass
