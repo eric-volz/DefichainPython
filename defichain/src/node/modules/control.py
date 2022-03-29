ad36e9dca9e84689f7d284831c33b7f79206824e
laptop <eric.volz02@gmail.com>
2022-03-30 00:34:33 +0200

    finished control commands without docs

--- defichain/src/node/modules/control.py
+++ defichain/src/node/modules/control.py
@@ -2,21 +2,21 @@ class Control:
     def __init__(self, node):
         self.node = node
 
-    def getmemoryinfo(self, mode=None):
+    def getmemoryinfo(self, mode=None):  # 01
         return self.node.rpc.call("getmemoryinfo", mode)
 
-    def getrpcinfo(self):
+    def getrpcinfo(self):  # 02
         return self.node.rpc.call("getrpcinfo")
 
-    def help(self, command=None):
+    def help(self, command=None):  # 03
         return self.node.rpc.call("help", command)
 
-    def logging(self, include=None, exclude=None):
+    def logging(self, include=None, exclude=None):  # 04
         include = [] if include is None else include
         return self.node.rpc.call("logging", include, exclude)
 
-    def stop(self):
+    def stop(self):  # 05
         return self.node.rpc.call("stop")
 
-    def uptime(self):
+    def uptime(self):  # 06
         return self.node.rpc.call("uptime")
