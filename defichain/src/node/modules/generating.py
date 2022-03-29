c4dc0ac41a79620744e73b065c6346589841f2d9
laptop <eric.volz02@gmail.com>
2022-03-30 00:34:55 +0200

    finished generating commands without docs

--- defichain/src/node/modules/generating.py
+++ defichain/src/node/modules/generating.py
@@ -1,3 +1,6 @@
 class Generating:
     def __init__(self, node):
         self.node = node
+
+    def generatetoaddress(self, nblocks, address, maxtries=None):  # 01
+        return self.node.rpc.call("generatetoaddress", nblocks, address, maxtries)
