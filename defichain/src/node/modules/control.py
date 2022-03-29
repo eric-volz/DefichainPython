0a544632bf85ee181a520cadad417c7eef6ee145
laptop <eric.volz02@gmail.com>
2022-03-29 18:18:43 +0200

    finished all commands from control without doc

--- defichain/src/node/modules/control.py
+++ defichain/src/node/modules/control.py
@@ -1,3 +1,22 @@
 class Control:
     def __init__(self, node):
         self.node = node
+
+    def getmemoryinfo(self, mode=None):
+        return self.node.rpc.call("getmemoryinfo", mode)
+
+    def getrpcinfo(self):
+        return self.node.rpc.call("getrpcinfo")
+
+    def help(self, command=None):
+        return self.node.rpc.call("help", command)
+
+    def logging(self, include=None, exclude=None):
+        include = [] if include is None else include
+        return self.node.rpc.call("logging", include, exclude)
+
+    def stop(self):
+        return self.node.rpc.call("stop")
+
+    def uptime(self):
+        return self.node.rpc.call("uptime")
