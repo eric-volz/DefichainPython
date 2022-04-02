1c6cf1b887a6ea61e771d8ffaf2d979bc9de8647
laptop <eric.volz02@gmail.com>
2022-04-02 23:52:40 +0200

    added util commands without docs

--- defichain/src/node/modules/util.py
+++ defichain/src/node/modules/util.py
@@ -1,3 +1,24 @@
 class Util:
     def __init__(self, node):
         self.node = node
+
+    def createmultisig(self, nrequired, keys, address_type=None):  # 01
+        return self.node.rpc.call("createmultisig", nrequired, keys, address_type)
+
+    def deriveaddresses(self, descriptor, range=None):  # 02
+        return self.node.rpc.call("deriveaddresses", descriptor, range)
+
+    def estimatesmartfee(self, conf_target, estimate_mode=None):  # 03
+        return self.node.rpc.call("estimatesmartfee", conf_target, estimate_mode)
+
+    def getdescriptorinfo(self):  # 04
+        pass
+
+    def signmessagewithprivkey(self):  # 05
+        pass
+
+    def validateaddress(self):  # 06
+        pass
+
+    def verifymessage(self):  # 07
+        pass
