c4675ae2f7ba1fa2422c9381d32f96d60faee5bd
laptop <eric.volz02@gmail.com>
2022-04-08 21:39:05 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Defichain Python Library'
 
 this_directory = path.abspath(path.dirname(__file__))
