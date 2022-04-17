18d062f61020555420562aeae50f9eb729df0cba
laptop <eric.volz02@gmail.com>
2022-04-17 17:35:55 +0200

    new connection Class for Ocean API

--- defichain/src/ocean/connection.py
+++ defichain/src/ocean/connection.py
@@ -1,28 +1,19 @@
-from urllib.request import urlopen
-from urllib.error import HTTPError
-
-
-TIMEOUT = 10
+import requests
 
 
 class Connection:
     def __init__(self, url):
         self.url = url
 
-    def request(self, ressource):
-        """
-        Requests Data from Ocean API.
-        @param ressource: string Data to be requested
-        @return: json
-        """
+    def call(self, data, size=None, next=None):
         try:
-            response = urlopen(self.url + ressource, timeout=TIMEOUT).read()
-            return response.decode("utf-8")
-        except HTTPError as e:
-            raise RequestException(e.read(), e.code)
-
-
-class RequestException(Exception):
-    def __init__(self, message, code):
-        Exception.__init__(self, message)
-        self.code = code
+            url = self.url + data
+            if size is not None and next is not None:
+                url += f"?size={size}&next={next}"
+            elif size is not None:
+                url += f"?size={size}"
+            elif next is not None:
+                url += f"?next={next}"
+            return requests.get(url)
+        except Exception as e:
+            raise Exception(e)
