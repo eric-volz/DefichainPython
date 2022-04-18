c35175d50c82674bb9a737574d6b72d02d31b983
laptop <eric.volz02@gmail.com>
2022-04-18 20:41:06 +0200

    finished ocean commands for oracles

--- defichain/src/ocean/modules/oracles.py
+++ defichain/src/ocean/modules/oracles.py
@@ -4,31 +4,12 @@ class Oracles:
     def __init__(self, ocean):
         self.ocean = ocean
 
-    def list(self, size=None, next=None) -> str:
-        """
-        Get a list of OceanOracles
-        @param size: how many oracles
-        @param next: next page hash
-        @return: json
-        """
-        size = 30 if size is None else size
-        next = "" if next is None else f"&next={next}"
-        addOn = f"?size={size}{next}"
-        request = f"oracles{addOn}"
-        return self.ocean.con.request(request)
+    def list(self, size=30, next=None):
+        return self.ocean._conn.call("oracles", size=size, next=next)
 
-    def getPriceFeed(self, oracleId: str, token: str, currency: str, size=None, next=None) -> str:
-        """
-        Get Price Feed for each Oracle by Price Ticker.
-        @param oracleId: oracle id found through list() function
-        @param token: ticker token like APPL
-        @param currency: currency like USD
-        @param size: ow many oracle feeds
-        @param next: next page hash
-        @return:
-        """
-        size = 30 if size is None else size
-        next = "" if next is None else f"&next={next}"
-        addOn = f"?size={size}{next}"
-        request = f"oracles/{oracleId}/{token}-{currency}/feed{addOn}"
-        return self.ocean.con.request(request)
+    def getPriceFeed(self, oracleId, token, currency, size=30, next=None):
+        key = f"{token}-{currency}"
+        return self.ocean._conn.call(f"oracles/{oracleId}/{key}/feed", size=size, next=next)
+
+    def getOracleByAddress(self, address):
+        return self.ocean._conn.call(f"oracles/{address}")
