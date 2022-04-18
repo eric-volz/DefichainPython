4fb4ca3025f74a9430f2003b2f855aaa440c633c
laptop <eric.volz02@gmail.com>
2022-04-19 01:16:26 +0200

    finished ocean commands for poolpairs

---
+++ defichain/src/ocean/modules/poolpairs.py
@@ -0,0 +1,30 @@
+# https://github.com/DeFiCh/jellyfish/blob/main/packages/whale-api-client/src/api/PoolPairs.ts
+
+class Poolpairs:
+    def __init__(self, ocean):
+        self.ocean = ocean
+
+    def list(self, size=30, next=None):  # 01
+        return self.ocean._conn.call(f"poolpairs", size=size, next=next)
+
+    def get(self, id):  # 02
+        return self.ocean._conn.call(f"poolpairs/{id}")
+
+    def listPoolSwaps(self, id, size=30, next=None):  # 03
+        return self.ocean._conn.call(f"poolpairs/{id}/swaps", size=size, next=next)
+
+    def listPoolSwapsVerbose(self, id, size=10, next=None):  # 04
+        return self.ocean._conn.call(f"poolpairs/{id}/swaps/verbose", size=size, next=next)
+
+    def listPoolSwapAggregates(self, id, interval,  size=30, next=None):  # 05
+        return self.ocean._conn.call(f"poolpairs/{id}/swaps/aggregate/{interval}", size=size, next=next)
+
+    def getSwappableTokens(self, tokenId):  # 06
+        return self.ocean._conn.call(f"poolpairs/paths/swappable/{tokenId}")
+
+    def getBestPath(self, fromTokenId, toTokenId):  # 07
+        return self.ocean._conn.call(f"poolpairs/paths/best/from/{fromTokenId}/to/{toTokenId}")
+
+    def getAllPaths(self, fromTokenId, toTokenId):  # 08
+        return self.ocean._conn.call(f"poolpairs/paths/from/{fromTokenId}/to/{toTokenId}")
+
