eff225f718d3b3ab8f3730507e5a32c63d2cf9c8
laptop <eric.volz02@gmail.com>
2022-04-20 19:44:48 +0200

    finished test of node connection

---
+++ tests/node/test_node.py
@@ -0,0 +1,20 @@
+import pytest
+from defichain import Node
+from util import load_secrets_conf
+
+
+secrets = load_secrets_conf()
+
+
+@pytest.mark.mandatory
+def test_createNode():
+    """
+    Checking if the Node Object builds as wanted
+    """
+    assert Node(user=secrets["user"], password=secrets["password"])
+    assert Node(user=secrets["user"], password=secrets["password"], url=secrets["url"])
+    assert Node(user=secrets["user"], password=secrets["password"], port=secrets["port"])
+    assert Node(user=secrets["user"], password=secrets["password"], url=secrets["url"], port=secrets["port"])
+    if "wallet_path" in secrets:
+        assert Node(user=secrets["user"], password=secrets["password"], url=secrets["url"], port=secrets["port"],
+                    wallet_path=secrets["wallet_path"])
