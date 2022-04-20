3bd99f2a0dc7c52df92a9bd25526cfa08ecff691
laptop <eric.volz02@gmail.com>
2022-04-20 19:43:42 +0200

    build testing structure

---
+++ tests/node/util.py
@@ -0,0 +1,31 @@
+from os import path
+from defichain import Node
+
+
+def load_secrets_conf():
+    if not path.isfile("secrets_conf.py"):
+        raise Exception("There is not secrets_conf.py File: \n"
+                        "To create one: read secrets_conf.example.py!")
+
+    from tests.secrets_conf import USER, PASSWORD, URL, PORT
+    json = {"user": USER, "password": PASSWORD, "url": URL, "port": PORT}
+
+    try:
+        from tests.secrets_conf import WALLET_PATH
+        json.update({"wallet_path": WALLET_PATH})
+    except:
+        pass
+    return json
+
+
+def createNode():
+    secrets = load_secrets_conf()
+
+    if "wallet_path" in secrets:
+        print("wallet")
+        return Node(user=secrets["user"], password=secrets["password"], url=secrets["url"], port=secrets["port"],
+                    wallet_path=secrets["wallet_path"])
+    print("hdöpahdöad")
+    return Node(user=secrets["user"], password=secrets["password"], url=secrets["url"], port=secrets["port"])
+
+test = createNode()
---
+++ tests/secrets_conf.example.py
@@ -0,0 +1,14 @@
+"""
+The place to store credentials for your Defichain Node
+
+--> Uncomment all variables and fill in your credentials
+--> Save the new file as secrets_conf.py
+"""
+# Mandatory
+# USER =
+# PASSWORD =
+# URL =
+# PORT =
+
+# Optional
+# WALLET_PATH =
