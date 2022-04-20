44c79cdf17fdf7dffa93b79329fb59edfc9fd6b8
laptop <eric.volz02@gmail.com>
2022-04-20 15:40:40 +0200

    correct error handling when using incorrect user or password

--- defichain/src/node/exceptions/ExceptionHandler.py
+++ defichain/src/node/exceptions/ExceptionHandler.py
@@ -15,22 +15,23 @@ from .ServiceUnavailable import ServiceUnavailable
 class ExceptionHandler:
     def __init__(self, response):
         self.statusCode = response.status_code
-        self.response = response.json()
 
-        if 'error' in self.response and self.response['error'] is not None:
-            code = self.response["error"]["code"]
-            msg = self.response["error"]["message"]
-            if self.statusCode == HTTPStatusCode.HTTP_BAD_REQUEST.value:
-                raise BadRequest(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_UNAUTHORIZED.value:
-                raise Unauthorized(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_FORBIDDEN.value:
-                raise Forbidden(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_NOT_FOUND.value:
-                raise NotFound(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_BAD_METHOD.value:
-                raise BadMethod(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_INTERNAL_SERVER_ERROR.value:
-                raise InternalServerError(code, msg)
-            elif self.statusCode == HTTPStatusCode.HTTP_SERVICE_UNAVAILABLE.value:
-                raise ServiceUnavailable(code, msg)
+        if self.statusCode == HTTPStatusCode.HTTP_UNAUTHORIZED.value:
+            raise Unauthorized()
+        else:
+            self.response = response.json()
+            if 'error' in self.response and self.response['error'] is not None:
+                code = self.response["error"]["code"]
+                msg = self.response["error"]["message"]
+                if self.statusCode == HTTPStatusCode.HTTP_BAD_REQUEST.value:
+                    raise BadRequest(code, msg)
+                elif self.statusCode == HTTPStatusCode.HTTP_FORBIDDEN.value:
+                    raise Forbidden(code, msg)
+                elif self.statusCode == HTTPStatusCode.HTTP_NOT_FOUND.value:
+                    raise NotFound(code, msg)
+                elif self.statusCode == HTTPStatusCode.HTTP_BAD_METHOD.value:
+                    raise BadMethod(code, msg)
+                elif self.statusCode == HTTPStatusCode.HTTP_INTERNAL_SERVER_ERROR.value:
+                    raise InternalServerError(code, msg)
+                elif self.statusCode == HTTPStatusCode.HTTP_SERVICE_UNAVAILABLE.value:
+                    raise ServiceUnavailable(code, msg)
--- defichain/src/node/exceptions/Unauthorized.py
+++ defichain/src/node/exceptions/Unauthorized.py
@@ -2,7 +2,5 @@ from .RPCErrorCode import RPCErrorCode
 
 
 class Unauthorized(Exception):
-    def __init__(self, code, msg):
-        self.message = msg
-        self.rpcErrorCode = RPCErrorCode(code).name
-        super().__init__(f"Unauthorized(401): {self.rpcErrorCode}: {self.message}")
+    def __init__(self):
+        super().__init__(f"Unauthorized(401): Authorization failed: Incorrect rpcuser or rpcpassword")
