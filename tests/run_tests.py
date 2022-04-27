49d3d18fd5a82536762c9fc95cd061e3b5064b1a
laptop <eric.volz02@gmail.com>
2022-04-27 13:31:25 +0200

    build a file to easily run tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,4 +1,43 @@
-import pytest
+import sys
+import os
 
-from node.test_node import *
-from node.test_blockchain import *
+MODULES = ["accounts", "blockchain", "control", "generating", "loan", "masternodes", "mining", "network", "oracles",
+           "poolpair", "rawtransaction", "spv", "tokens", "util", "vault", "wallet", "zmq", "node", "exceptions"]
+MARKER = ["mandatory", "query", "transaction"]
+
+# Parameter Handling
+args = sys.argv
+if len(args) == 1:
+    module = "all"
+    marker = "all"
+elif len(args) == 3:
+    module = args[1]
+    marker = args[2]
+else:
+    raise Exception("Use none or both parameters, not just one!\n"
+                    "1. Parameter Modules: name of module to test or all for every module\n"
+                    "2. Parameter Marker: name of marker to do the tests on or all for every marker\n")
+
+if module != "all" and not module in MODULES:
+    raise Exception(f"The passed parameter is not supported: {module}")
+if marker != "all" and not marker in MARKER:
+    raise Exception(f"The passed parameter is not supported: {marker}")
+
+# Test Handling
+if module == "all" and marker == "all":
+    stream = os.popen("pytest node")
+    output = stream.read()
+    print(output)
+elif module == "all":
+    stream = os.popen(f"pytest node -k {marker}")
+    output = stream.read()
+    print(output)
+elif marker == "all":
+    stream = os.popen(f"pytest node/test_{module}.py")
+    output = stream.read()
+    print(output)
+else:
+    stream = os.popen(f"pytest node/test_{module}.py -k {marker}")
+    output = stream.read()
+    print(output)
+
