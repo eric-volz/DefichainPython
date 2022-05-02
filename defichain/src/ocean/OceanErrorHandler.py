90d40acb417f23e7a31b40a6fd59c069954a9f84
laptop <eric.volz02@gmail.com>
2022-05-02 11:46:15 +0200

    ocean error handler, with support of all exceptions

--- defichain/src/ocean/OceanErrorHandler.py
+++ defichain/src/ocean/OceanErrorHandler.py
@@ -6,29 +6,37 @@ from defichain.src.exceptions.Unauthorized import Unauthorized
 from defichain.src.exceptions.Forbidden import Forbidden
 from defichain.src.exceptions.NotFound import NotFound
 from defichain.src.exceptions.BadMethod import BadMethod
+from defichain.src.exceptions.UnprocessableEntity import UnprocessableEntity
 from defichain.src.exceptions.InternalServerError import InternalServerError
 from defichain.src.exceptions.ServiceUnavailable import ServiceUnavailable
+
+
+STATUS_CODES_WITH_ERROR = [400, 401, 403, 404, 405, 422, 500, 503]
 
 
 class OceanErrorHandler:
     def __init__(self, response):
         self.statusCode = response.status_code
+        print(self.statusCode)
 
-        if self.statusCode == HTTPStatusCode.HTTP_UNAUTHORIZED.value:
-            raise Unauthorized()
-        else:
-            self.response_text = response.json()
-            if 'error' in self.response_text and self.response_text['error'] is not None:
-                msg = self.response_text["error"]
-                if self.statusCode == HTTPStatusCode.HTTP_BAD_REQUEST.value:
-                    raise BadRequest(f": {msg}")
-                elif self.statusCode == HTTPStatusCode.HTTP_FORBIDDEN.value:
-                    raise Forbidden(f": {msg}")
-                elif self.statusCode == HTTPStatusCode.HTTP_NOT_FOUND.value:
-                    raise NotFound(f": {msg}")
-                elif self.statusCode == HTTPStatusCode.HTTP_BAD_METHOD.value:
-                    raise BadMethod(f": {msg}")
-                elif self.statusCode == HTTPStatusCode.HTTP_INTERNAL_SERVER_ERROR.value:
-                    raise InternalServerError(f": {msg}")
-                elif self.statusCode == HTTPStatusCode.HTTP_SERVICE_UNAVAILABLE.value:
-                    raise ServiceUnavailable(f": {msg}")
+        if self.statusCode in STATUS_CODES_WITH_ERROR:
+            if self.statusCode == 401:
+                raise Unauthorized()
+            else:
+                self.response_text = response.json()
+                if 'error' in self.response_text and self.response_text['error'] is not None:
+                    msg = self.response_text["error"]
+                    if self.statusCode == 400:
+                        raise BadRequest(f"{msg}")
+                    if self.statusCode == 403:
+                        raise Forbidden(f"{msg}")
+                    if self.statusCode == 404:
+                        raise NotFound(f"{msg}")
+                    if self.statusCode == 405:
+                        raise BadMethod(f"{msg}")
+                    if self.statusCode == 422:
+                        raise UnprocessableEntity(f"{msg}")
+                    if self.statusCode == 500:
+                        raise InternalServerError(f"{msg}")
+                    if self.statusCode == 503:
+                        raise ServiceUnavailable(f"{msg}")
