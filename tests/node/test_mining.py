41f3a94381f6a1e609966778706d21e56a8797fc
laptop <eric.volz02@gmail.com>
2022-05-05 17:23:28 +0200

    better tests for submitblock and submitheader

--- tests/node/test_mining.py
+++ tests/node/test_mining.py
@@ -44,18 +44,28 @@ def test_prioritisetransaction():  # 05
 
 @pytest.mark.query
 def test_submitblock():  # 06
-    hex="048230"
-    string = ".* RPC_DESERIALIZATION_ERROR: Block decode failed"
-    with pytest.raises(InternalServerError, match=string):
-        assert node.mining.submitblock(hex)
-        assert node.mining.submitblock(hex, "ignored")
-        assert node.mining.submitblock(hexdata=hex, dummy="ignored")
+    hex="000000202d120af2bdbd115199f04ce2369457f29809c5c76bfc2b3631f7e715002f5865934e2276696daba9f3f93a5c81ff8be17" \
+        "164ab0d49c668fe2b19daafc9caee964be97362be7534185ca48cd64f8f118a9c59cb38d71f0648d4d892566548e2a13289dbdc2e" \
+        "fdb22919531c00000000004e000000000000004120de510ff026945302ce0ce8abd9bde6ebb77bd8805c65abca1ec4ea402666aa7" \
+        "6313f618a7c4e9445cb2323add0dd2979ff8c4ce843c1c9964d471c20f9032b140104000000000101000000000000000000000000" \
+        "0000000000000000000000000000000000000000ffffffff050319531c00ffffffff033b8180ef010000001976a914c5dbd91efad" \
+        "0e2ccc02942cea009fa12e0bac3b488ac0046a7fe480000000017a914dd7730517e0e4969b4e43677ff5bee682e53420a87000000" \
+        "000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf9000120000000000" \
+        "000000000000000000000000000000000000000000000000000000000000000"
+    assert node.mining.submitblock(hex)
+    assert node.mining.submitblock(hex, "ignored")
+    assert node.mining.submitblock(hexdata=hex, dummy="ignored")
 
 
 @pytest.mark.query
 def test_submitheader():  # 06
-    hex = "048230"
-    string = ".* RPC_DESERIALIZATION_ERROR: Block header decode failed"
-    with pytest.raises(InternalServerError, match=string):
-        assert node.mining.submitheader(hex)
-        assert node.mining.submitheader(hexdata=hex)
+    hex = "000000202d120af2bdbd115199f04ce2369457f29809c5c76bfc2b3631f7e715002f5865934e2276696daba9f3f93a5c81ff8be17" \
+          "164ab0d49c668fe2b19daafc9caee964be97362be7534185ca48cd64f8f118a9c59cb38d71f0648d4d892566548e2a13289dbdc2e" \
+          "fdb22919531c00000000004e000000000000004120de510ff026945302ce0ce8abd9bde6ebb77bd8805c65abca1ec4ea402666aa7" \
+          "6313f618a7c4e9445cb2323add0dd2979ff8c4ce843c1c9964d471c20f9032b140104000000000101000000000000000000000000" \
+          "0000000000000000000000000000000000000000ffffffff050319531c00ffffffff033b8180ef010000001976a914c5dbd91efad" \
+          "0e2ccc02942cea009fa12e0bac3b488ac0046a7fe480000000017a914dd7730517e0e4969b4e43677ff5bee682e53420a87000000" \
+          "000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf9000120000000000" \
+          "000000000000000000000000000000000000000000000000000000000000000"
+    assert node.mining.submitheader(hex) is None
+    assert node.mining.submitheader(hexdata=hex) is None
