ce895b550ff956683c4a4f6172532583c303756f
laptop <eric.volz02@gmail.com>
2022-05-08 14:51:29 +0200

    adjust parameter

--- tests/ocean/test_blocks.py
+++ tests/ocean/test_blocks.py
@@ -1,17 +1,16 @@
 import pytest
 from defichain import Ocean
 
-# Import Exceptions
-from defichain.exceptions.InternalServerError import InternalServerError
-
 ocean = Ocean()
+SIZE = 30
+NEXT = None
 
 
 @pytest.mark.query
 def test_list():  # 01
     assert ocean.blocks.list()
-    assert ocean.blocks.list(30, None)
-    assert ocean.blocks.list(size=30, next=None)
+    assert ocean.blocks.list(SIZE, NEXT)
+    assert ocean.blocks.list(size=SIZE, next=NEXT)
 
 
 @pytest.mark.query
@@ -28,6 +27,6 @@ def test_get():  # 02
 def test_getTransactions():  # 03
     blockhash = "b6e0ee6da7e61f6b672b72b9a8a68413cdf0344102acecccf9cf2a6d338630c6"
     assert ocean.blocks.getTransactions(blockhash)
-    assert ocean.blocks.getTransactions(blockhash, 30, None)
-    assert ocean.blocks.getTransactions(hash=blockhash, size=30, next=None)
+    assert ocean.blocks.getTransactions(blockhash, SIZE, NEXT)
+    assert ocean.blocks.getTransactions(hash=blockhash, size=SIZE, next=NEXT)
 
