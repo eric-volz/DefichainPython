449ee5986a21dbb505c13db1f0adb4b35c24ccef
laptop <eric.volz02@gmail.com>
2022-05-09 15:11:45 +0200

    finish tests for ocean tokens

---
+++ tests/ocean/test_tokens.py
@@ -0,0 +1,20 @@
+import pytest
+from defichain import Ocean
+
+ocean = Ocean()
+SIZE = 30
+NEXT = None
+
+
+@pytest.mark.query
+def test_list():  # 01
+    assert ocean.tokens.list()
+    assert ocean.tokens.list(SIZE, NEXT)
+    assert ocean.tokens.list(size=SIZE, next=NEXT)
+
+
+@pytest.mark.query
+def test_get():  # 02
+    id = 0  # DFI
+    assert ocean.tokens.get(id)
+    assert ocean.tokens.get(id=id)
