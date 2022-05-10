2c09152c457991dd8e6f81f5c8ced61960f060b3
laptop <eric.volz02@gmail.com>
2022-05-10 16:30:26 +0200

    fix raw Transaction: is now able to send signed transactions to ocean servers

--- defichain/ocean/modules/rawTx.py
+++ defichain/ocean/modules/rawTx.py
@@ -1,11 +1,20 @@
 # https://github.com/DeFiCh/jellyfish/blob/main/packages/whale-api-client/src/api/RawTx.ts
+import json
+from defichain.node.util import BuildJson
+
 
 class RawTx:
     def __init__(self, ocean):
         self._ocean = ocean
 
-    def send(self, rawTx):
-        return self._ocean._conn.post("rawtx/send", [rawTx])
+    def send(self, hex, maxFeeRate=None):
+        j = BuildJson()
+        j.append("hex", hex)
+        j.append("maxFeeRate", maxFeeRate)
+        return self._ocean._conn.post("rawtx/send", json.dumps(j.build()))
 
-    def test(self, rawTx):
-        return self._ocean._conn.post("rawtx/test", [rawTx])
+    def test(self, hex, maxFeeRate=None):
+        j = BuildJson()
+        j.append("hex", hex)
+        j.append("maxFeeRate", maxFeeRate)
+        return self._ocean._conn.post("rawtx/test", json.dumps(j.build()))
