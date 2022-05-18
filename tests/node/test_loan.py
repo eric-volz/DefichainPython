653d585506ded118031eed2b1d5e4fc89e6bf6e3
laptop <eric.volz02@gmail.com>
2022-05-18 17:02:51 +0200

    almost finish test for loan: one method has to be corrected in main

--- tests/node/test_loan.py
+++ tests/node/test_loan.py
@@ -68,3 +68,83 @@ def test_listcollateraltokens():  # 08
     assert node.loan.listcollateraltokens()
     assert node.loan.listcollateraltokens(0, True)
     assert node.loan.listcollateraltokens(height=0, all=True)
+
+
+@pytest.mark.query
+def test_listloanschemes():  # 09
+    assert node.loan.listloanschemes()
+
+
+@pytest.mark.query
+def test_listloantokens():  # 10
+    assert node.loan.listloantokens()
+
+
+@pytest.mark.transactions
+def test_paybackloan():  # 11
+    amounts = ["0.00000001@DUSD", "0.00000001@DUSD"]
+    loans = [{"dToken": "DUSD", "amounts": "0.00000001@DUSD"}]
+    assert node.loan.paybackloan(vault, address, amounts)
+    assert node.loan.paybackloan(vaultId=vault, _from=address, amounts=amounts)
+    assert node.loan.paybackloan(vault, address, None, loans)
+    assert node.loan.paybackloan(vaultId=vault, _from=address, loans=loans)
+
+
+@pytest.mark.query
+def test_setcollateraltoken():  # 12
+    string = ".* RPC_INVALID_PARAMETER: Token TEST does not exist!"
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setcollateraltoken("TEST", 150, "TEST/USD")
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setcollateraltoken("TEST", 150, "TEST/USD", node.blockchain.getblockcount()+1, [])
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setcollateraltoken(token="TEST", factor=150, fixedIntervalPriceId="TEST/USD",
+                                        activateAfterBlock=node.blockchain.getblockcount()+1, inputs=[])
+
+
+@pytest.mark.query
+def test_setdefaultloanscheme():  # 13
+    string = ".* RPC_INVALID_ADDRESS_OR_KEY: Need foundation member authorization"
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setdefaultloanscheme("MIN150")
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setdefaultloanscheme("MIN150", [])
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setdefaultloanscheme(id="MIN150", inputs=[])
+
+
+@pytest.mark.query
+def test_setloantoken():  # 14
+    string = ".* RPC_INVALID_ADDRESS_OR_KEY: Need foundation member authorization"
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setloantoken("TEST", "TEST/USD")
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setloantoken("TEST", "TEST/USD", "TEST Coin", True, 0, [])
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.setloantoken(symbol="TEST", fixedIntervalPriceId="TEST/USD", name="TEST Coin", mintable=True,
+                                      interest=0, inputs=[])
+
+
+@pytest.mark.transactions
+def test_takeloan():  # 15
+    amounts = ["0.00000001@DUSD", "0.00000001@DUSD"]
+    assert node.loan.takeloan(vault, amounts)
+    assert node.loan.takeloan(vault, amounts, address, [])
+    assert node.loan.takeloan(vaultId=vault, amounts=amounts, to=address, inputs=[])
+
+
+@pytest.mark.query
+def test_updateloanscheme():  # 16
+    string = ".* RPC_INVALID_ADDRESS_OR_KEY: Need foundation member authorization"
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.updateloanscheme(100, 10, "MIN1000")
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.updateloanscheme(100, 10, "MIN1000", node.blockchain.getblockcount()+1, [])
+    with pytest.raises(InternalServerError, match=string):
+        assert node.loan.updateloanscheme(mincolratio=100, interestrate=10, id="MIN1000",
+                                          ACTIVATE_AFTER_BLOCK=node.blockchain.getblockcount()+1, inputs=[])
+
+
+@pytest.mark.query
+def test_updateloantoken():  # 17
+    pass  # has to be corrected in main and then a test will be added
