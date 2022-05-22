0b58f62fea413644b0481efa2f0a0e07bd80c68e
laptop <eric.volz02@gmail.com>
2022-05-22 11:32:21 +0200

    more information in setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from os import path
 VERSION = '0.0.8'
 DESCRIPTION = 'Defichain Python Library'
 
+# Project URLs
+project_urls = {
+    "Tracker": "https://github.com/eric-volz/DefichainPython",
+    "Documentation": "https://defichain.readthedocs.io"
+}
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README_for_pypi.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
@@ -27,7 +33,11 @@ setup(
     install_requires=["requests"],
     keywords=['python', 'defichain', 'node', 'ocean'],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
