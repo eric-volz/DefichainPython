18127371ec32b7cbc3db36adb1ec735faa9dafcb
laptop <eric.volz02@gmail.com>
2022-05-25 19:42:40 +0200

    small changes on sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from typing import Any, Dict
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -31,7 +32,7 @@ release = 'v0.0.8'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,9 +49,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+#html_theme = 'alabaster'
 html_theme = 'furo'
+html_title = f"DefichainPython {release}"
+language = "en"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
+html_static_path = ["_static"]
