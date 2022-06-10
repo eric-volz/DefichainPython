9379acfdaf17d086a49c513a9f2d67c13edfb8cf
laptop <eric.volz02@gmail.com>
2022-06-10 19:51:19 +0200

    change print output from exceptions

--- defichain/exceptions/ServiceUnavailable.py
+++ defichain/exceptions/ServiceUnavailable.py
@@ -1,4 +1,4 @@
 class ServiceUnavailable(Exception):
     def __init__(self, msg):
         self.message = msg
-        super().__init__(f"Service Unavailable(503): {self.message}")
+        super().__init__(f"ServiceUnavailable(503): {self.message}")
