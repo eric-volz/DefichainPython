f1bfd1ee720a427f20f3c0ea570beae5a65307db
laptop <eric.volz02@gmail.com>
2022-07-18 18:49:58 +0200

    finish docs for blockchain module

--- defichain/node/modules/blockchain.py
+++ defichain/node/modules/blockchain.py
@@ -824,23 +824,166 @@ class Blockchain:
         """
         return self._node._rpc.call("preciousblock", blockhash)
 
-    def pruneblockchain(self, height):  # 26
+    def pruneblockchain(self, height: int) -> int:  # 26
+        """
+        pruneblockchain height
+
+        :param height: (required) The block height to prune up to. May be set to a discrete height, or a unix timestamp
+                                  to prune blocks whose block time is at least 2 hours older than the provided timestamp.
+        :type height: int
+        :return: n (numeric) Height of the last block pruned
+
+        :example:
+
+            >>> node.blockchain.pruneblockchain(1000)
+        """
         return self._node._rpc.call("pruneblockchain", height)
 
-    def savemempool(self):  # 27
+    def savemempool(self) -> None:  # 27
+        """
+        Dumps the mempool to disk. It will fail until the previous dump is fully loaded.
+
+        :example:
+
+            >>> node.blockchain.savemempool()
+        """
         return self._node._rpc.call("savemempool")
 
-    def scantxoutset(self, action, scanobjects):  # 28
+    def scantxoutset(self, action: str, scanobjects: [{}]) -> {}:  # 28
+        """
+        EXPERIMENTAL warning: this call may be removed or changed in future releases.
+
+        Scans the unspent transaction output set for entries that match certain output descriptors.
+
+        Examples of output descriptors are:
+
+            addr(<address>)                      Outputs whose scriptPubKey corresponds to the specified address (does not include P2PK)
+
+            raw(<hex script>)                    Outputs whose scriptPubKey equals the specified hex scripts
+
+            combo(<pubkey>)                      P2PK, P2PKH, P2WPKH, and P2SH-P2WPKH outputs for the given pubkey
+
+            pkh(<pubkey>)                        P2PKH outputs for the given pubkey
+
+            sh(multi(<n>,<pubkey>,<pubkey>,...)) P2SH-multisig outputs for the given threshold and pubkeys
+
+        In the above, <pubkey> either refers to a fixed public key in hexadecimal notation, or to an xpub/xprv optionally followed by one
+        or more path elements separated by "/", and optionally ending in "/*" (unhardened), or "/*'" or "/*h" (hardened) to specify all
+        unhardened or hardened child keys.
+
+        In the latter case, a range needs to be specified by below if different from 1000.
+        For more information on output descriptors, see the documentation in the doc/descriptors.md file.
+
+        :param action: (required) The action to execute
+
+                                  "start" for starting a scan
+
+                                  "abort" for aborting the current scan (returns true when abort was successful)
+
+                                  "status" for progress report (in %) of the current scan
+        :type action: str
+        :param scanobjects: (required) Every scan object is either a string descriptor or an object:
+
+            .. code-block:: text
+
+                [
+                    "descriptor",             (string) An output descriptor
+                    {                         (json object) An object with output descriptor and metadata
+                    "desc": "str",          (string, required) An output descriptor
+                    "range": n or [n,n],    (numeric or array, optional, default=1000) The range of HD chain indexes to explore (either end or [begin,end])
+                    },
+                    ...
+                ]
+        :type scanobjects: [{}]
+        :return:
+
+            .. code-block:: text
+
+                {
+                    "unspents": [
+                        {
+                        "txid" : "transactionid",     (string) The transaction id
+                        "vout": n,                    (numeric) the vout value
+                        "scriptPubKey" : "script",    (string) the script key
+                        "desc" : "descriptor",        (string) A specialized descriptor for the matched scriptPubKey
+                        "amount" : x.xxx,             (numeric) The total amount in DFI of the unspent output
+                        "height" : n,                 (numeric) Height of the unspent transaction output
+                        }
+                    ,...],
+                    "total_amount" : x.xxx,          (numeric) The total amount of all found unspent outputs in DFI
+                }
+        """
         return self._node._rpc.call("scantxoutset", action, scanobjects)
 
-    def setgov(self, variables, inputs=None):  # 29
+    def setgov(self, variables: {}, inputs: [{}] = None) -> str:  # 29
+        """
+        Set special 'governance' variables:: ATTRIBUTES, ICX_TAKERFEE_PER_BTC, LP_LOAN_TOKEN_SPLITS, LP_SPLITS, ORACLE_BLOCK_INTERVAL, ORACLE_DEVIATION
+
+        :param variables: (required) Object with variables
+
+            .. code-block:: text
+
+                {
+                    "name": "str",      (string, required) Variable's name is the key, value is the data. Exact data type depends on variable's name.
+                }
+
+        :type variables: {}
+        :param inputs: (optional) :ref:`Node Inputs`
+        :type inputs: [{}]
+        :return: "hash" (string) The hex-encoded hash of broadcasted transaction
+
+        :example:
+
+            >>> node.blockchain.setgov({"LP_SPLITS": {"2":0.2,"3":0.8})
+        """
         return self._node._rpc.call("setgov", variables, inputs)
 
-    def setgovheight(self, variables, height, inputs=None):  # 30
+    def setgovheight(self, variables: {}, height: int, inputs: [{}] = None):  # 30
+        """
+        Change governance variable at height: ATTRIBUTES, ICX_TAKERFEE_PER_BTC, LP_LOAN_TOKEN_SPLITS, LP_SPLITS, ORACLE_DEVIATION
+
+        :param variables: (required) Object with variable
+
+            .. code-block:: text
+
+                {
+                    "name": "str",      (string, required)  Variable name is the key, value is the data. Exact data type depends on variable name.
+                }
+
+        :type variables: {}
+        :param height: (required) Start height for the changes to take effect.
+        :type height: int
+        :param inputs: (optional) :ref:`Node Inputs`
+        :type inputs: [{}]
+        :return: "hash" (string) The hex-encoded hash of broadcasted transaction
+
+        :example:
+
+            >>> node.blockchain.setgovheight({"LP_SPLITS": {"2":0.2,"3":0.8}, 3000000)
+        """
         return self._node._rpc.call("setgovheight", variables, height, inputs)
 
-    def verifychain(self, checklevel=3, nblocks=6):  # 31
+    def verifychain(self, checklevel: int = 3, nblocks: int = 6) -> bool:  # 31
+        """
+        Verifies blockchain database.
+
+        :param checklevel: (optional, range=0-4) How thorough the block verification is
+        :type checklevel: int
+        :param nblocks: (optional, 0=all) The number of blocks to check
+        :type nblocks: int
+        :return: true|false (boolean) Verified or not
+
+        :example:
+
+            >>> node.blockchain.verifychain()
+        """
         return self._node._rpc.call("verifychain", checklevel, nblocks)
 
-    def verifytxoutproof(self, proof):  # 32
+    def verifytxoutproof(self, proof: str) -> [str]:  # 32
+        """
+
+        :param proof: (required) The hex-encoded proof generated by gettxoutproof
+        :type proof: str
+        :return: ["txid"] (array, strings) The txid(s) which the proof commits to, or empty array if the proof can not be validated
+        """
         return self._node._rpc.call("verifytxoutproof", proof)
