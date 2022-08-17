d93b7bd3391c6348916cdbf2448f31381d70bb21
laptop <eric.volz02@gmail.com>
2022-08-17 04:11:07 +0200

    docs for generating

--- defichain/node/modules/generating.py
+++ defichain/node/modules/generating.py
@@ -2,5 +2,22 @@ class Generating:
     def __init__(self, node):
         self._node = node
 
-    def generatetoaddress(self, nblocks, address, maxtries=1):  # 01
+    def generatetoaddress(self, nblocks: int, address: str, maxtries: int = 1) -> ["str"]:  # 01
+        """
+        Mine blocks immediately to a specified address (before the RPC call returns)
+
+        :param nblocks: (required) How many blocks are generated immediately
+        :type nblocks: int
+        :param address: (required) The address to send the newly generated DFI to
+        :type address: str
+        :param maxtries: (optional) How many iterations to try
+        :type maxtries: int
+        :return: (array) hashes of blocks generated
+
+        :example:
+
+        Generate 11 blocks to myaddress
+
+        >>> node.generating.generatetoaddress(11, address)
+        """
         return self._node._rpc.call("generatetoaddress", nblocks, address, maxtries)
