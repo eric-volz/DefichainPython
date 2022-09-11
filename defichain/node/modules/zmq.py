6557e7680445baef92d4c1edb06878ec5e651934
laptop <eric.volz02@gmail.com>
2022-09-11 13:51:48 +0200

    finish docs for node zmq

--- defichain/node/modules/zmq.py
+++ defichain/node/modules/zmq.py
@@ -2,5 +2,25 @@ class Zmq:
     def __init__(self, node):
         self._node = node
 
-    def getzmqnotifications(self):
+    def getzmqnotifications(self) -> {}:
+        """
+        Returns information about the active ZeroMQ notifications
+
+        :return:
+
+            .. code-block:: text
+
+                [
+                    {                        (json object)
+                        "type": "pubhashtx", (string) Type of notification
+                        "address": "...",    (string) Address of the publisher
+                        "hwm": n             (numeric) Outbound message high water mark
+                    },
+                    ...
+                ]
+
+        :example:
+
+            >>> node.control.getzmqnotifications()
+        """
         return self._node._rpc.call("getzmqnotifications")
