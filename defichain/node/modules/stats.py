338b2d6b3819d9ae2317b9f5443b2293391e703d
laptop <eric.volz02@gmail.com>
2022-09-11 13:44:08 +0200

    finish docs for node stats

--- defichain/node/modules/stats.py
+++ defichain/node/modules/stats.py
@@ -5,8 +5,66 @@ class Stats:
     def __init__(self, node):
         self._node = node
 
-    def getrpcstats(self, command):
+    def getrpcstats(self, command: str) -> {}:
+        """
+        Get RPC stats for selected command
+
+        :param command: (required) The command to get stats for
+        :type command: str
+        :return:
+
+            .. code-block:: text
+
+                {
+                    "name":               (string) The RPC command name.
+                    "latency":            (json object) Min, max and average latency.
+                    "payload":            (json object) Min, max and average payload size in bytes.
+                    "count":              (numeric) The number of times this command as been used.
+                    "lastUsedTime":       (numeric) Last used time as timestamp.
+                    "history":            (json array) History of last 5 RPC calls.
+                    [
+                        {
+                        "timestamp": (numeric)
+                        "latency":   (numeric)
+                        "payload":   (numeric)
+                        }
+                    ]
+                }
+
+            :example:
+
+                >>> node.stats.getrpcstats("getblockcount")
+        """
         return self._node._rpc.call("getrpcstats", command)
 
-    def listrpcstats(self):
+    def listrpcstats(self) -> []:
+        """
+        List used RPC commands
+
+        :return:
+
+            .. code-block:: text
+
+                [
+                    {
+                        "name":               (string) The RPC command name.
+                        "latency":            (json object) Min, max and average latency.
+                        "payload":            (json object) Min, max and average payload size in bytes.
+                        "count":              (numeric) The number of times this command as been used.
+                        "lastUsedTime":       (numeric) Last used time as timestamp.
+                        "history":            (json array) History of last 5 RPC calls.
+                        [
+                            {
+                                "timestamp": (numeric)
+                                "latency":   (numeric)
+                                "payload":   (numeric)
+                            }
+                        ]
+                    }
+                ]
+
+            :example:
+
+                >>> node.stats.listrpcstats()
+        """
         return self._node._rpc.call("listrpcstats")
