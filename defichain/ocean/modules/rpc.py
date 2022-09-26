300bf2ad6d280955b53e13916f0eb2d75fa6b116
eric_volz <eric@volz.link>
2022-09-26 16:04:02 +0200

    finish docs for rpc

--- defichain/ocean/modules/rpc.py
+++ defichain/ocean/modules/rpc.py
@@ -4,7 +4,20 @@ class Rpc:
     def __init__(self, ocean):
         self._ocean = ocean
 
-    def call(self, method, *params):
+    def call(self, method: str, *params: []) -> {}:
+        """
+        Call an RPC method
+
+        :param method: (required) method of the RPC method
+        :type method: str
+        :param params: (optinal) params to send upstream
+        :type params: array
+        :return: json array
+
+        :example:
+
+        >>> ocean.rpc.call("getblockcount")
+        """
         if params == ():
             return self._ocean._conn.post(f"rpc/{method}", [])
         else:
