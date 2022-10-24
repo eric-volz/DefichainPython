156c765e9a923e7a001cebbafe63126bbbc30ee0
eric_volz <eric@volz.link>
2022-10-24 13:23:11 +0200

    version update for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Intr0c'
 author = 'Intr0c'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.2.0'
+release = 'v2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
