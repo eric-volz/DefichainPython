99f3e94005e940ab288e2bf20bc13d164863220b
eric_volz <eric@volz.link>
2022-10-30 15:21:50 +0100

    rework docs for stats

--- defichain/node/modules/stats.py
+++ defichain/node/modules/stats.py
@@ -11,41 +11,10 @@ class Stats:
 
         :param command: (required) The command to get stats for
         :type command: str
-        :return:
+        :return: {...} (json) -- returns rpc stats
 
-        .. code-block:: text
+            .. code-block:: text
 
-            {
-                "name":               (string) The RPC command name.
-                "latency":            (json object) Min, max and average latency.
-                "payload":            (json object) Min, max and average payload size in bytes.
-                "count":              (numeric) The number of times this command as been used.
-                "lastUsedTime":       (numeric) Last used time as timestamp.
-                "history":            (json array) History of last 5 RPC calls.
-                [
-                    {
-                    "timestamp": (numeric)
-                    "latency":   (numeric)
-                    "payload":   (numeric)
-                    }
-                ]
-            }
-
-        :example:
-
-        >>> node.stats.getrpcstats("getblockcount")
-        """
-        return self._node._rpc.call("getrpcstats", command)
-
-    def listrpcstats(self) -> []:
-        """
-        List used RPC commands
-
-        :return:
-
-        .. code-block:: text
-
-            [
                 {
                     "name":               (string) The RPC command name.
                     "latency":            (json object) Min, max and average latency.
@@ -55,16 +24,47 @@ class Stats:
                     "history":            (json array) History of last 5 RPC calls.
                     [
                         {
-                            "timestamp": (numeric)
-                            "latency":   (numeric)
-                            "payload":   (numeric)
+                        "timestamp": (numeric)
+                        "latency":   (numeric)
+                        "payload":   (numeric)
                         }
                     ]
                 }
-            ]
 
         :example:
 
-        >>> node.stats.listrpcstats()
+            >>> node.stats.getrpcstats("getblockcount")
+        """
+        return self._node._rpc.call("getrpcstats", command)
+
+    def listrpcstats(self) -> []:
+        """
+        List used RPC commands
+
+        :return: {...} (json) -- returns a list of rpc stats
+
+            .. code-block:: text
+
+                [
+                    {
+                        "name":               (string) The RPC command name.
+                        "latency":            (json object) Min, max and average latency.
+                        "payload":            (json object) Min, max and average payload size in bytes.
+                        "count":              (numeric) The number of times this command as been used.
+                        "lastUsedTime":       (numeric) Last used time as timestamp.
+                        "history":            (json array) History of last 5 RPC calls.
+                        [
+                            {
+                                "timestamp": (numeric)
+                                "latency":   (numeric)
+                                "payload":   (numeric)
+                            }
+                        ]
+                    }
+                ]
+
+        :example:
+
+            >>> node.stats.listrpcstats()
         """
         return self._node._rpc.call("listrpcstats")
