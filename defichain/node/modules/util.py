6cced3337a7dbbf83a7c8ef720e950b902ed3c1a
eric_volz <eric@volz.link>
2022-10-30 15:21:56 +0100

    rework docs for util

--- defichain/node/modules/util.py
+++ defichain/node/modules/util.py
@@ -14,24 +14,25 @@ class Util:
         :type keys: json array
         :param address_type: (optional) The address type to use. Options are "legacy", "p2sh-segwit", and "bech32"
         :type address_type: str
-        :return:
+        :return: {...} (json) -- returns a json object with the address and redeemScript
 
-        .. code-block:: text
+            .. code-block:: text
 
-            {
-                "address":"multisigaddress",  (string) The value of the new multisig address.
-                "redeemScript":"script"       (string) The string value of the hex-encoded redemption script.
-            }
+                {
+                    "address":"multisigaddress",  (string) The value of the new multisig address.
+                    "redeemScript":"script"       (string) The string value of the hex-encoded redemption script.
+                }
 
         :example:
 
-        >>> node.util.createmultisig(2, ["03789ed0bb717d88f7d321a368d905e7430207ebbd82bd342cf11ae157a7ace5fd", "03dbc6764b8884a92e871274b87583e6d5c2a58819473e17e107ef3f6aa5a61626"])
+            >>> node.util.createmultisig(2, ["03789ed0bb717d88f7d321a368d905e7430207ebbd82bd342cf11ae157a7ace5fd", "03dbc6764b8884a92e871274b87583e6d5c2a58819473e17e107ef3f6aa5a61626"])
         """
         return self._node._rpc.call("createmultisig", nrequired, keys, address_type)
 
     def deriveaddresses(self, descriptor: str, range: int or [] = None) -> [str]:  # 02
         """
         Derives one or more addresses corresponding to an output descriptor.
+
         Examples of output descriptors are:
 
             pkh(<pubkey>)                        P2PKH outputs for the given pubkey
@@ -44,17 +45,18 @@ class Util:
 
         In the above, <pubkey> either refers to a fixed public key in hexadecimal notation, or to an xpub/xprv
         optionally followed by one or more path elements separated by "/", where "h" represents a hardened child key.
+
         For more information on output descriptors, see the documentation in the doc/descriptors.md file.
 
         :param descriptor: (required) The descriptor
         :type descriptor: str
         :param range: (optional) If a ranged descriptor is used, this specifies the end or the range (in [begin,end] notation) to derive
         :type range: int or array
-        :return: [ address ] (array) the derived addresses
+        :return: [...] (array) -- the derived addresses
 
         :example:
 
-        >>> node.util.deriveaddresses("wpkh([d34db33f/84h/0h/0h]xpub6DJ2dNUysrn5Vt36jH2KLBT2i1auw1tTSSomg8PhqNiUtx8QX2SvC9nrHu81fT41fvDUnhMjEzQgXnQjKEu3oaqMSzhSrHMxyyoEAmUHQbY/0/*)#cjjspncu", [0,2])
+            >>> node.util.deriveaddresses("wpkh([d34db33f/84h/0h/0h]xpub6DJ2dNUysrn5Vt36jH2KLBT2i1auw1tTSSomg8PhqNiUtx8QX2SvC9nrHu81fT41fvDUnhMjEzQgXnQjKEu3oaqMSzhSrHMxyyoEAmUHQbY/0/*)#cjjspncu", [0,2])
         """
         return self._node._rpc.call("deriveaddresses", descriptor, range)
 
@@ -69,33 +71,40 @@ class Util:
         :param conf_target: (required) Confirmation target in blocks (1 - 1008)
         :type conf_target: int
         :param estimate_mode: (optional) The fee estimate mode.
-                                         Whether to return a more conservative estimate which also satisfies
-                                         a longer history. A conservative estimate potentially returns a
-                                         higher feerate and is more likely to be sufficient for the desired
-                                         target, but is not as responsive to short term drops in the
-                                         prevailing fee market.  Must be one of:
-                                         "UNSET"
-                                         "ECONOMICAL"
-                                         "CONSERVATIVE"
+             Whether to return a more conservative estimate which also satisfies
+             a longer history. A conservative estimate potentially returns a
+             higher feerate and is more likely to be sufficient for the desired
+             target, but is not as responsive to short term drops in the
+             prevailing fee market.
+
+             Must be one of:
+
+             "UNSET"
+
+             "ECONOMICAL"
+
+             "CONSERVATIVE"
+
         :type estimate_mode: str
-        :return:
+        :return: {...} (json) -- resturns a estimation of a fee
 
-        .. code-block:: text
+            .. code-block:: text
 
-            {
-              "feerate" : x.x,     (numeric, optional) estimate fee rate in DFI/kB
-              "errors": [ str... ] (json array of strings, optional) Errors encountered during processing
-              "blocks" : n         (numeric) block number where estimate was found
-            }
+                {
+                  "feerate" : x.x,     (numeric, optional) estimate fee rate in DFI/kB
+                  "errors": [ str... ] (json array of strings, optional) Errors encountered during processing
+                  "blocks" : n         (numeric) block number where estimate was found
+                }
 
-        The request target will be clamped between 2 and the highest target
-        fee estimation is able to return based on how long it has been running.
-        An error is returned if not enough transactions and blocks
-        have been observed to make an estimate for any number of blocks.
+            The request target will be clamped between 2 and the highest target
+            fee estimation is able to return based on how long it has been running.
+
+            An error is returned if not enough transactions and blocks
+            have been observed to make an estimate for any number of blocks.
 
         :example:
 
-        >>> node.util.estimatesmartfee(6)
+            >>> node.util.estimatesmartfee(6)
         """
         return self._node._rpc.call("estimatesmartfee", conf_target, estimate_mode)
 
@@ -105,21 +114,21 @@ class Util:
 
         :param descriptor: (required) The descriptor
         :type descriptor: str
-        :return:
+        :return: {...} (json) -- returns information about decriptor
 
-        .. code-block:: text
+            .. code-block:: text
 
-            {
-                "descriptor" : "desc",         (string) The descriptor in canonical form, without private keys
-                "checksum" : "chksum",         (string) The checksum for the input descriptor
-                "isrange" : true|false,        (boolean) Whether the descriptor is ranged
-                "issolvable" : true|false,     (boolean) Whether the descriptor is solvable
-                "hasprivatekeys" : true|false, (boolean) Whether the input descriptor contained at least one private key
-            }
+                {
+                    "descriptor" : "desc",         (string) The descriptor in canonical form, without private keys
+                    "checksum" : "chksum",         (string) The checksum for the input descriptor
+                    "isrange" : true|false,        (boolean) Whether the descriptor is ranged
+                    "issolvable" : true|false,     (boolean) Whether the descriptor is solvable
+                    "hasprivatekeys" : true|false, (boolean) Whether the input descriptor contained at least one private key
+                }
 
         :example:
 
-        >>> node.util.getdescriptorinfo("wpkh([d34db33f/84h/0h/0h]0279be667ef9dcbbac55a06295Ce870b07029Bfcdb2dce28d959f2815b16f81798)")
+            >>> node.util.getdescriptorinfo("wpkh([d34db33f/84h/0h/0h]0279be667ef9dcbbac55a06295Ce870b07029Bfcdb2dce28d959f2815b16f81798)")
         """
         return self._node._rpc.call("getdescriptorinfo", descriptor)
 
@@ -131,11 +140,11 @@ class Util:
         :type privkey: str
         :param message: (required) The message to create a signature of
         :type message: str
-        :return: "signature" (string) The signature of the message encoded in base 64
+        :return: "signature" (string) -- The signature of the message encoded in base 64
 
         :example:
 
-        >>> node.util.signmessagewithprivkey("privkey", "my message")
+            >>> node.util.signmessagewithprivkey("privkey", "my message")
         """
         return self._node._rpc.call("signmessagewithprivkey", privkey, message)
 
@@ -145,24 +154,24 @@ class Util:
 
         :param address: (required) The defi address to validate
         :type address: str
-        :return:
+        :return: {...} (json) -- Return information about the given defi address
 
-        .. code-block:: text
+            .. code-block:: text
 
-            {
-              "isvalid" : true|false,       (boolean) If the address is valid or not. If not, this is the only property returned.
-              "address" : "address",        (string) The defi address validated
-              "scriptPubKey" : "hex",       (string) The hex-encoded scriptPubKey generated by the address
-              "isscript" : true|false,      (boolean) If the key is a script
-              "iswitness" : true|false,     (boolean) If the address is a witness address
-              "witness_version" : version   (numeric, optional) The version number of the witness program
-              "witness_program" : "hex"     (string, optional) The hex value of the witness program
-            }
+                {
+                  "isvalid" : true|false,       (boolean) If the address is valid or not. If not, this is the only property returned.
+                  "address" : "address",        (string) The defi address validated
+                  "scriptPubKey" : "hex",       (string) The hex-encoded scriptPubKey generated by the address
+                  "isscript" : true|false,      (boolean) If the key is a script
+                  "iswitness" : true|false,     (boolean) If the address is a witness address
+                  "witness_version" : version   (numeric, optional) The version number of the witness program
+                  "witness_program" : "hex"     (string, optional) The hex value of the witness program
+                }
 
 
         :example:
 
-        >>> node.util.validateaddress("df1qduwqfyhz0n0duvmudlhmyx2uzk4u2xqmn627zr")
+            >>> node.util.validateaddress("df1qduwqfyhz0n0duvmudlhmyx2uzk4u2xqmn627zr")
         """
         return self._node._rpc.call("validateaddress", address)
 
@@ -176,10 +185,10 @@ class Util:
         :type signature: str
         :param message: (required) The message that was signed
         :type message: str
-        :return: true|false   (boolean) If the signature is verified or not
+        :return: bool -- If the signature is verified or not
 
         :example:
 
-        >>> node.util.verifymessage("df1qduwqfyhz0n0duvmudlhmyx2uzk4u2xqmn627zr", "signature", "my message")
+            >>> node.util.verifymessage("df1qduwqfyhz0n0duvmudlhmyx2uzk4u2xqmn627zr", "signature", "my message")
         """
         return self._node._rpc.call("verifymessage", address, signature, message)
