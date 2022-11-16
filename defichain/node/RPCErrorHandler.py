5b83dd7d5a1b65192960790b552a0c4bd4bd9624
eric_volz <eric@volz.link>
2022-11-16 23:54:45 +0100

    adding logging for node

--- defichain/node/RPCErrorHandler.py
+++ defichain/node/RPCErrorHandler.py
@@ -1,6 +1,6 @@
 # RPC Error Codes
 # https://github.com/bitcoin/bitcoin/blob/master/src/rpc/protocol.h
-
+from defichain.logger import Logger
 from defichain.exceptions.http.RPCErrorCode import RPCErrorCode
 
 from defichain.exceptions.http.BadRequest import BadRequest
@@ -15,11 +15,14 @@ STATUS_CODES_WITH_ERROR = [400, 401, 403, 404, 405, 500, 503]
 
 
 class RPCErrorHandler:
-    def __init__(self, response):
+    def __init__(self, response, logger: Logger):
         self.statusCode = response.status_code
+        self.logger = logger
 
         if self.statusCode in STATUS_CODES_WITH_ERROR:
             if self.statusCode == 401:
+                if logger:
+                    self.logger.error("NodeError", f"Unauthorized")
                 raise Unauthorized()
             else:
                 self.response_text = response.json()
@@ -28,14 +31,26 @@ class RPCErrorHandler:
                     rpc_name = RPCErrorCode(rpc_code).name
                     msg = self.response_text["error"]["message"]
                     if self.statusCode == 400:
+                        if logger:
+                            self.logger.error("NodeError", f"BadRequest: {rpc_name}: {msg}")
                         raise BadRequest(f"{rpc_name}: {msg}")
                     if self.statusCode == 403:
+                        if logger:
+                            self.logger.error("NodeError", f"Forbidden: {rpc_name}: {msg}")
                         raise Forbidden(f"{rpc_name}: {msg}")
                     if self.statusCode == 404:
+                        if logger:
+                            self.logger.error("NodeError", f"NotFound: {rpc_name}: {msg}")
                         raise NotFound(f"{rpc_name}: {msg}")
                     if self.statusCode == 405:
+                        if logger:
+                            self.logger.error("NodeError", f"BadMethod: {rpc_name}: {msg}")
                         raise BadMethod(f"{rpc_name}: {msg}")
                     if self.statusCode == 500:
+                        if logger:
+                            self.logger.error("NodeError", f"InternalServerError: {rpc_name}: {msg}")
                         raise InternalServerError(f"{rpc_name}: {msg}")
                     if self.statusCode == 503:
+                        if logger:
+                            self.logger.error("NodeError", f"ServiceUnavailable: {rpc_name}: {msg}")
                         raise ServiceUnavailable(f"{rpc_name}: {msg}")
--- defichain/node/rpc.py
+++ defichain/node/rpc.py
@@ -1,15 +1,17 @@
 import json
 import requests
 import time
+from defichain.logger import Logger
 from defichain.node.RPCErrorHandler import RPCErrorHandler
 from defichain.exceptions.http.ServiceUnavailable import ServiceUnavailable
 
 
 class RPC(object):
-    def __init__(self, url):
+    def __init__(self, url, logger: Logger):
         self._session = requests.Session()
         self._url = url
         self._headers = {'content-type': 'application/json'}
+        self._logger = logger
 
     def call(self, rpc_method, *params):
         filtered_params = []
@@ -20,6 +22,12 @@ class RPC(object):
         payload = json.dumps({"method": rpc_method, "params": list(filtered_params), "jsonrpc": "2.0"})
         tries = 3
         hadConnectionFailures = False
+
+        # Logging of Ocean get request url
+        if self._logger:
+            if self._logger.log_level == "input" or self._logger.log_level == "all":
+                self._logger.input("NodeInput", f"Node request URL: {self._url} | Headers: {self._headers} | Payload: {payload}")
+
         while True:
             try:
                 response = self._session.post(self._url, headers=self._headers, data=payload)
@@ -35,5 +43,13 @@ class RPC(object):
                     print('Connected for remote procedure call after retry.')
                 break
 
-        RPCErrorHandler(response)  # Check for Exceptions
-        return response.json()['result']
+        RPCErrorHandler(response, self._logger)  # Check for Exceptions
+
+        result = response.json()['result']
+
+        # Logging of Ocean get request result
+        if self._logger:
+            if self._logger.log_level == "output" or self._logger.log_level == "all":
+                self._logger.output("NodeOutput", f"Node requests result: {result}")
+
+        return result
