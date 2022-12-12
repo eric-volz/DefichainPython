4b62ae176cc45982e0ab67efcba906baa738d5fa
eric-volz <eric@volz.link>
2022-12-12 23:43:16 +0100

    new release v2.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Intr0c'
 author = 'Intr0c'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.2.0'
+release = 'v2.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from os import path
 
-VERSION = '2.2.0'
+VERSION = '2.3.0'
 DESCRIPTION = 'Defichain Python Library'
 
 # Project URLs
