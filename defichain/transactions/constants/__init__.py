54d8654b5247db1b3acfee3cd1a3606acea70d4f
eric-volz <eric@volz.link>
2023-01-15 23:47:02 +0100

    split constants to different files

---
+++ defichain/transactions/constants/__init__.py
@@ -0,0 +1,12 @@
+
+# Address
+from .address import CHARSET, CHARSET_BASE, AddressTypes, MAX_OP_LENGTH
+
+# Defi Transaction
+from .defitx import DefiTx_SIGNATURE, DefiTxType
+
+# OP Codes
+from .opcodes import OPCodes
+
+# Raw Transactions
+from .rawtransactions import SEQUENCE, SCRIPTSIG, SIGHASH, ORDER
---
+++ defichain/transactions/constants/address.py
@@ -0,0 +1,17 @@
+
+
+# Charsets
+CHARSET: str = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+CHARSET_BASE = len(CHARSET)
+
+
+# Address Types
+class AddressTypes:
+    P2PKH: str = "P2PKH"
+    P2SH: str = "P2SH"
+    P2WPKH: str = "P2WPKH"
+    P2WSH: str = "P2WSH"
+
+
+# Max Script Length
+MAX_OP_LENGTH = 76
