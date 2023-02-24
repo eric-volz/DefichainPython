fffd017878404609e354706e7a4409999f455652
eric-volz <eric@volz.link>
2023-02-24 19:11:54 +0100

    tokenId constants for mainnet and testnet

--- defichain/transactions/constants/tokens.py
+++ defichain/transactions/constants/tokens.py
@@ -1,8 +1,7 @@
 import json
 import os
 from defichain import Ocean
-
-ocean = Ocean()
+from defichain.networks import DefichainMainnet, DefichainTestnet
 
 
 class TokenTypes:
@@ -16,23 +15,23 @@ class Tokens:
     PATH = os.path.join(os.path.dirname(__file__))
 
     @staticmethod
-    def get_standardTokens() -> [{}]:
-        with open(Tokens.PATH + f"/{TokenTypes.STANDARD}_tokens.json", "r") as f:
+    def get_standardTokens(network: DefichainMainnet or DefichainTestnet) -> [{}]:
+        with open(f"{Tokens.PATH}/{network.NETWORK}/{TokenTypes.STANDARD}_tokens.json", "r") as f:
             return json.loads(f.read())
 
     @staticmethod
-    def get_liquidityTokens() -> [{}]:
-        with open(Tokens.PATH + f"/{TokenTypes.LIQUIDITY}_tokens.json", "r") as f:
+    def get_liquidityTokens(network: DefichainMainnet or DefichainTestnet) -> [{}]:
+        with open(f"{Tokens.PATH}/{network.NETWORK}/{TokenTypes.LIQUIDITY}_tokens.json", "r") as f:
             return json.loads(f.read())
 
     @staticmethod
-    def get_loanTokens() -> [{}]:
-        with open(Tokens.PATH + f"/{TokenTypes.LOAN}_tokens.json", "r") as f:
+    def get_loanTokens(network: DefichainMainnet or DefichainTestnet) -> [{}]:
+        with open(f"{Tokens.PATH}/{network.NETWORK}/{TokenTypes.LOAN}_tokens.json", "r") as f:
             return json.loads(f.read())
 
     @staticmethod
-    def get_customTokens() -> [{}]:
-        with open(Tokens.PATH + f"/{TokenTypes.CUSTOM}_tokens.json", "r") as f:
+    def get_customTokens(network: DefichainMainnet or DefichainTestnet) -> [{}]:
+        with open(f"{Tokens.PATH}/{network.NETWORK}/{TokenTypes.CUSTOM}_tokens.json", "r") as f:
             return json.loads(f.read())
 
     @staticmethod
@@ -41,36 +40,46 @@ class Tokens:
         Asks the ocean API for the newest tokens and prints the results in json files into the current folder
         """
 
-        standard_tokens = []
-        liquidity_tokens = []
-        loan_tokens = []
-        custom_token = []
+        networks = (DefichainMainnet.NETWORK, DefichainTestnet.NETWORK)
 
-        i = -1
-        tokens = True
-        while tokens:
-            tokens = ocean.tokens.list(size=200, next=i)["data"]
-            for token in tokens:
-                token = {"id": token["id"], "symbol": token["symbol"], "name": token["name"], "isDAT": token["isDAT"],
-                         "isLPS": token["isLPS"], "isLoanToken": token["isLoanToken"]}
-                if token["isDAT"] and not token["isLPS"]:
-                    standard_tokens.append(token)
-                if token["isDAT"] and token["isLPS"]:
-                    liquidity_tokens.append(token)
-                if token["isDAT"] and token["isLoanToken"]:
-                    loan_tokens.append(token)
-                if not token["isDAT"] and not token["isLPS"]:
-                    custom_token.append(token)
-            i += len(tokens)
+        for network in networks:
+            ocean = Ocean(network=network)
 
-        with open(f"{TokenTypes.STANDARD}_tokens.json", "w") as f:
-            f.write(json.dumps(standard_tokens))
-        with open(f"{TokenTypes.LIQUIDITY}_tokens.json", "w") as f:
-            f.write(json.dumps(liquidity_tokens))
-        with open(f"{TokenTypes.LOAN}_tokens.json", "w") as f:
-            f.write(json.dumps(loan_tokens))
-        with open(f"{TokenTypes.CUSTOM}_tokens.json", "w") as f:
-            f.write(json.dumps(custom_token))
+            standard_tokens = []
+            liquidity_tokens = []
+            loan_tokens = []
+            custom_token = []
+
+            i = -1
+            tokens = True
+            while tokens:
+                tokens = ocean.tokens.list(size=200, next=i)["data"]
+                for token in tokens:
+                    token = {"id": token["id"], "symbol": token["symbol"], "name": token["name"], "isDAT": token["isDAT"],
+                             "isLPS": token["isLPS"], "isLoanToken": token["isLoanToken"]}
+                    if token["isDAT"] and not token["isLPS"]:
+                        standard_tokens.append(token)
+                    if token["isDAT"] and token["isLPS"]:
+                        liquidity_tokens.append(token)
+                    if token["isDAT"] and token["isLoanToken"]:
+                        loan_tokens.append(token)
+                    if not token["isDAT"] and not token["isLPS"]:
+                        custom_token.append(token)
+                i += len(tokens)
+
+            # Create directory
+            if not os.path.exists(f"{Tokens.PATH}/{network}/"):
+                os.mkdir(f"{Tokens.PATH}/{network}/")
+
+            # Write data into files
+            with open(f"{Tokens.PATH}/{network}/{TokenTypes.STANDARD}_tokens.json", "w") as f:
+                f.write(json.dumps(standard_tokens))
+            with open(f"{Tokens.PATH}/{network}/{TokenTypes.LIQUIDITY}_tokens.json", "w") as f:
+                f.write(json.dumps(liquidity_tokens))
+            with open(f"{Tokens.PATH}/{network}/{TokenTypes.LOAN}_tokens.json", "w") as f:
+                f.write(json.dumps(loan_tokens))
+            with open(f"{Tokens.PATH}/{network}/{TokenTypes.CUSTOM}_tokens.json", "w") as f:
+                f.write(json.dumps(custom_token))
 
 
 if __name__ == "__main__":
--- defichain/transactions/utils/token.py
+++ defichain/transactions/utils/token.py
@@ -1,4 +1,5 @@
 from defichain import Ocean
+from defichain.networks import DefichainMainnet, DefichainTestnet
 from defichain.exceptions.transactions import TokenError
 from defichain.transactions.constants.tokens import Tokens, TokenTypes
 
@@ -8,49 +9,49 @@ ocean = Ocean()
 class Token:
 
     @staticmethod
-    def _get_tokens(TYPE: str) -> [{}]:
+    def _get_tokens(network: DefichainMainnet or DefichainTestnet, TYPE: str) -> [{}]:
         if TYPE == TokenTypes.STANDARD:
-            return Tokens.get_standardTokens()
+            return Tokens.get_standardTokens(network)
         elif TYPE == TokenTypes.LIQUIDITY:
-            return Tokens.get_liquidityTokens()
+            return Tokens.get_liquidityTokens(network)
         elif TYPE == TokenTypes.LOAN:
-            return Tokens.get_loanTokens()
+            return Tokens.get_loanTokens(network)
         elif TYPE == TokenTypes.CUSTOM:
-            return Tokens.get_customTokens()
+            return Tokens.get_customTokens(network)
         else:
             raise TokenError("The given tokens type is not valid.\n"
                              "Use the token types in defichain.transaction.constance.tokens.")
 
     @staticmethod
-    def get_symbol_from_id(id: int, TYPE: str) -> str:
-        tokens = Token._get_tokens(TYPE)
+    def get_symbol_from_id(network: DefichainMainnet or DefichainTestnet, tokenId: int, TYPE: str) -> str:
+        tokens = Token._get_tokens(network, TYPE)
         for token in tokens:
-            if token["id"] == str(id):
+            if token["id"] == str(tokenId):
                 return token["symbol"]
-        raise TokenError(f"The given id: {id} does not exist. Check your token id input.")
+        raise TokenError(f"The given id: {tokenId} does not exist. Check your token id input.")
 
     @staticmethod
-    def get_id_from_symbol(symbol: str, TYPE: str) -> int:
-        tokens = Token._get_tokens(TYPE)
+    def get_id_from_symbol(network: DefichainMainnet or DefichainTestnet, symbol: str, TYPE: str) -> int:
+        tokens = Token._get_tokens(network, TYPE)
         for token in tokens:
             if token["symbol"] == str(symbol):
                 return token["id"]
         raise TokenError(f"The given symbol: {symbol} does not exist. Check your input.")
 
     @staticmethod
-    def get_name_from_id(id: int, TYPE: str) -> str:
-        tokens = Token._get_tokens(TYPE)
+    def get_name_from_id(network: DefichainMainnet or DefichainTestnet, tokenId: int, TYPE: str) -> str:
+        tokens = Token._get_tokens(network, TYPE)
         for token in tokens:
-            if token["id"] == str(id):
+            if token["id"] == str(tokenId):
                 return token["name"]
-        raise TokenError(f"The given id: {id} does not exist. Check your token id input.")
+        raise TokenError(f"The given id: {tokenId} does not exist. Check your token id input.")
 
     @staticmethod
-    def verify_tokenId(tokenId: int) -> bool:
+    def verify_tokenId(network: DefichainMainnet or DefichainTestnet, tokenId: int) -> bool:
         tokenTypes = [TokenTypes.STANDARD, TokenTypes.LOAN, TokenTypes.LIQUIDITY, TokenTypes.CUSTOM]
         for _type in tokenTypes:
             try:
-                Token.get_name_from_id(tokenId, _type)
+                Token.get_name_from_id(network, tokenId, _type)
                 return True
             except:
                 pass
