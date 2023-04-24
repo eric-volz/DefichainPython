f2fa323c68262270c5d58307e6d58fbb0fb1f48d
eric-volz <eric@volz.link>
2023-04-24 23:19:18 +0200

    add documentation for consortium

--- defichain/ocean/modules/consortium.py
+++ defichain/ocean/modules/consortium.py
@@ -6,9 +6,69 @@ class Consortium:
         self._ocean = ocean
 
     def getAssetBreakdown(self):
+        """
+        Gets the asset breakdown information of consortium members
+
+        :return: Json String
+
+            .. code-block::
+
+                {
+                    tokenId: string
+                    id: string
+                    name: string
+                    backingAddresses: string[]
+                    minted: string
+                    burned: string
+                }
+
+        :example:
+
+        >>> ocean.consortium.getAssetBreakdown()
+        """
 
         return self._ocean._conn.get("consortium/assetbreakdown")
 
     def getMemberStats(self, memberId: str):
+        """
+        Gets the stats information of a specific consortium member
+
+        :param memberId: (required) member id of the consortium member
+        :type memberId: str
+        :return: Json String
+
+            .. code-block::
+
+                {
+                    memberId: string
+                    memberName: string
+                    mintTokens:
+                    [
+                        {
+                        tokenSymbol: string
+                        tokenDisplaySymbol: string
+                        tokenId: string
+                        member:
+                            {
+                                minted: string
+                                mintedDaily: string
+                                mintLimit: string
+                                mintDailyLimit: string
+                            }
+                        token:
+                            {
+                                minted: string
+                                mintedDaily: string
+                                mintLimit: string
+                                mintDailyLimit: string
+                            }
+                        }
+                    ]
+                }
+
+        :example:
+
+        >>> ocean.consortium.getMemberStats("memberId")
+        """
 
         return self._ocean._conn.get(f"consortium/stats/{memberId}")
