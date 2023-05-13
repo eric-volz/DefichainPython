749c41a1dd86dc1a274dddffb1c95612b60de111
eric-volz <eric@volz.link>
2023-05-13 14:24:14 +0200

    improve error handling of rpc

--- defichain/node/rpc.py
+++ defichain/node/rpc.py
@@ -6,6 +6,9 @@ from defichain.node.RPCErrorHandler import RPCErrorHandler
 from defichain.exceptions.http.ServiceUnavailable import ServiceUnavailable
 
 
+RPC_TIMEOUT = 60
+
+
 class RPC(object):
     def __init__(self, url, logger: Logger):
         self._session = requests.Session()
@@ -26,18 +29,26 @@ class RPC(object):
         # Logging of Node get request url
         if self._logger:
             if self._logger.log_level == "input" or self._logger.log_level == "all":
-                self._logger.input("NodeInput", f"Node request URL: {self._url} | Headers: {self._headers} | Payload: {payload}")
+                self._logger.input("NodeInput", f"Node request URL: {self._url} | Headers: {self._headers} | Payload: "
+                                                f"{payload}")
 
         while True:
             try:
-                response = self._session.post(self._url, headers=self._headers, data=payload)
+                response = self._session.post(self._url, headers=self._headers, data=payload, timeout=RPC_TIMEOUT)
             except requests.exceptions.ConnectionError as e:
                 tries -= 1
                 if tries == 0:
-                    raise ServiceUnavailable(e)
+                    raise ServiceUnavailable("The service you are trying to connect to is not available")
                 hadFailedConnections = True
-                print(f"Couldn't connect for remote procedure call, will sleep for five seconds and then try again ({tries} more tries)")
+                print(f"Couldn't connect for remote procedure call, will sleep for five seconds and then try again "
+                      f"({tries} more tries)")
+                if self._logger:
+                    self._logger.error("ConnectionError", f"Could not connect to the Node, trying again")
                 time.sleep(5)
+            except requests.exceptions.ReadTimeout as e:
+                if self._logger:
+                    self._logger.error("ConnectionError", f"The connection timed out: {e}")
+
             else:
                 if hadConnectionFailures:
                     print('Connected for remote procedure call after retry.')
