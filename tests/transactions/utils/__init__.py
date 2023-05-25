e8649a6f0a5b75976a477136e1b8ecbc404f56e2
eric-volz <eric@volz.link>
2023-05-25 20:27:04 +0200

    finish tests for utils calculate

---
+++ tests/transactions/utils/__init__.py
@@ -0,0 +1,10 @@
+
+
+class TestCalculate:
+    DHASH: str = "ea07687190f4a6a974710eaaac483fad6436deb73d6a9297c76cdb658054b2d4"
+    INT: int = 65535
+    VARINT: str = "82fe7f"
+    COMPACTSIZE: str = "fdffff"
+    ADDRESSAMOUNT: str = {'df1qsqa3yt6sfkx3nu5ch88ymlzwnwd5wkkstywjvu': ['232@DFI', '234@DFI'],
+                          '8bXZh4A9NWW9weWW7nUpLJhAMmfLL6o5wo': '32432@DFI'}
+
