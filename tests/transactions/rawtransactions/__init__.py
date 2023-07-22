fe901c04bc1704200b9894a1060de6aaa089ee06
eric-volz <eric@volz.link>
2023-07-23 01:00:02 +0200

    test from specified mnemonic

--- tests/transactions/rawtransactions/__init__.py
+++ tests/transactions/rawtransactions/__init__.py
@@ -1,14 +1,24 @@
 from defichain.networks import DefichainMainnet
+from defichain import Wallet
+from defichain.transactions.utils import Converter
 from defichain.transactions.rawtransactions import Transaction, TxP2PKHInput, TxP2WPKHInput, TxP2SHInput
-from defichain.transactions.rawtransactions import TxOutput, TxAddressOutput, TxDataOutput, TxDefiOutput
+from defichain.transactions.rawtransactions import TxAddressOutput
 from defichain.transactions.defitx import UtxosToAccount
-from defichain.transactions.rawtransactions import estimate_fee
+
+
+class Keys:
+    mnemonic = "zone myth seat market badge hamster puppy worry mind attend heavy churn upgrade heart scan address " \
+               "across mix spread add boss armed desert mixed"
+    wallet: Wallet = Wallet(DefichainMainnet)
+    wallet.from_mnemonic(mnemonic)
+    privateKey: str = wallet.private_key()
+    wif: str = wallet.wif()
 
 
 class Addresses:
-    P2PKH: str = "8P2Y9mePZEWDFZje5azzrdEcCfaHYXQf9g"
-    P2SH: str = "dKjQW9MaCCswbWnuSGXrjWGT5Mhd7bW5eF"
-    P2WPKH: str = "df1q29x7wpn8flcqgscre4xfjgp99mppcmaus83uzt"
+    P2PKH: str = Keys.wallet.p2pkh_address()
+    P2SH: str = Keys.wallet.p2sh_address()
+    P2WPKH: str = Keys.wallet.p2wpkh_address()
 
 
 class TestInputs:
@@ -19,8 +29,8 @@ class TestInputs:
     P2PKH_Input = TxP2PKHInput(TXID, VOUT, Addresses.P2PKH)
     P2PKH_Input_Serialized = "061ca2791086a0df7c4786bbf16b933873d47ef0029ea539ce7e23d398fcbc3d0100000000ffffffff"
     P2SH_Input = TxP2SHInput(TXID, VOUT, Addresses.P2WPKH, VALUE)
-    P2SH_Input_Serialized = "061ca2791086a0df7c4786bbf16b933873d47ef0029ea539ce7e23d398fcbc3d0100000017160014514de706" \
-                            "674ff0044303cd4c9920252ec21c6fbcffffffff"
+    P2SH_Input_Serialized = "061ca2791086a0df7c4786bbf16b933873d47ef0029ea539ce7e23d398fcbc3d0100000017160014ad56321" \
+                            "e69b7e2d30aeca9f49979ffc53084296fffffffff"
     P2WPKH_Input = TxP2WPKHInput(TXID, VOUT, Addresses.P2WPKH, VALUE)
     P2WPKH_Input_Serialized = "061ca2791086a0df7c4786bbf16b933873d47ef0029ea539ce7e23d398fcbc3d0100000000ffffffff"
 
@@ -30,17 +40,17 @@ class TestOutputs:
     DATA: str = "#ROADTO50"
     Data_Output_Serialized: str = "00000000000000000b6a0923524f4144544f353000"
 
-    DefiTx = UtxosToAccount("df1q29x7wpn8flcqgscre4xfjgp99mppcmaus83uzt", 1)
+    DefiTx = UtxosToAccount(Addresses.P2WPKH, 1)
     DefiTx_Serialized: str = "6a2a446654785501160014514de706674ff0044303cd4c9920252ec21c6fbc01000000000100000000000000"  # UtxoToAccount DefiTx
-    DefiTx_Output_Serialized: str = "01000000000000002c6a2a446654785501160014514de706674ff0044303cd4c9920252ec21c6fbc" \
+    DefiTx_Output_Serialized: str = "01000000000000002c6a2a446654785501160014ad56321e69b7e2d30aeca9f49979ffc53084296f" \
                                     "0100000000010000000000000000"
 
     P2PKH_Address_Output = TxAddressOutput(VALUE, Addresses.P2PKH)
-    P2PKH_Address_Output_Serialized = "f4010000000000001976a9145720415061b4e2feb76eebcb3a9940ae4413373588ac00"
+    P2PKH_Address_Output_Serialized = "f4010000000000001976a914ad56321e69b7e2d30aeca9f49979ffc53084296f88ac00"
     P2SH_Address_Output = TxAddressOutput(VALUE, Addresses.P2SH)
-    P2SH_Address_Output_Serialized = "f40100000000000017a914453dfd6b24118d99ec767d3b6188dce50e6705b38700"
+    P2SH_Address_Output_Serialized = "f40100000000000017a91493a457d0e4cc789beb65eb77742d35297652dafe8700"
     P2WPKH_Address_Output = TxAddressOutput(VALUE, Addresses.P2WPKH)
-    P2WPKH_Address_Output_Serialized = "f401000000000000160014514de706674ff0044303cd4c9920252ec21c6fbc00"
+    P2WPKH_Address_Output_Serialized = "f401000000000000160014ad56321e69b7e2d30aeca9f49979ffc53084296f00"
 
 
 class TestFee:
