2335e1c468871fee853226615e9bc3207af408a4
eric-volz <eric@volz.link>
2023-08-09 00:18:35 +0200

    update docs for builder vault

--- defichain/transactions/builder/modules/vault.py
+++ defichain/transactions/builder/modules/vault.py
@@ -4,20 +4,37 @@ from defichain.transactions.builder.rawtransactionbuilder import RawTransactionB
 
 
 class Vault:
+    """
+    **The methods of this module create vault transactions**
+
+    1. **createvault**: transaction for creating a vault
+
+    2. **deposittovault**: transaction for depositing collateral token to a vault
+
+    3. **withdrawfromvault**: transaction for withdraw of collateral from a vault
+    """
 
     def __init__(self, builder):
         self._builder: RawTransactionBuilder = builder
 
     def createvault(self, ownerAddress: str, schemeId: str, inputs=[]) -> Transaction:
         """
-        Creates a transaction for creating a vault
+        Creates a transaction for creating a vault.
+
+        The creation of a vault will cost you 1 UTXO as a fee by the blockchain.
+
+        SchemeIDs mainnet: MIN1000, MIN500, MIN350, MIN200, MIN175, MIN150
+        SchemeIDs testnet: C1000, C500, C350, C200, C175, C150
+
+        >>> builder.vault.createvault("df1qw8c57c3c4u7k2h4gv2d5x4jr4qgq6cugg33g6e", "MIN150") # creates a new vault
 
         :param ownerAddress: (required) the address where vault will be created
         :type ownerAddress: str
         :param schemeId: (required) the scheme id for the vault
         :type schemeId: str
-        :param inputs: (optional) Inputs
-        :type inputs: TxInput
+        :param inputs: (optional) additional inputs to spend
+        :type inputs: [TxInput]
+        :return: Transaction
         """
 
         defiTx = CreateVault(ownerAddress, schemeId)
@@ -25,16 +42,18 @@ class Vault:
 
     def deposittovault(self, vaultId: str, addressFrom: str, amount: str, inputs=[]) -> Transaction:
         """
-        Creates a transaction for depositing collateral token amount to vault
+        Creates a transaction for depositing collateral token to a vault
+
+        >>> builder.vault.deposittovault("5cbe99407674a689fa9b8a522462b7a4b3e7893f61453ce3fa77f1307f7d0600", "df1qw8c57c3c4u7k2h4gv2d5x4jr4qgq6cugg33g6e", "100@DFI") # deposits collateral to a vault
 
         :param vaultId: (required) vault id
         :type vaultId: str
         :param addressFrom: (required) address containing collateral
         :type addressFrom: str
-        :param amount: (required) Amount of collateral in amount@symbol format
-        :type amount: str
-        :param inputs: (optional) Inputs
-        :type inputs: TxInput
+        :param amount: (required) string with amount and token separated by an @
+        :type amount: :ref:`Transactions Amounts`
+        :param inputs: (optional) additional inputs to spend
+        :type inputs: [TxInput]
         :return: Transaction
         """
 
@@ -46,16 +65,18 @@ class Vault:
 
     def withdrawfromvault(self, vaultId: str, addressTo: str, amount: str, inputs=[]) -> Transaction:
         """
-        Creates a transaction for withdraw of collateral token from a vault
+        Creates a transaction for withdraw of collateral from a vault
+
+        >>> builder.vault.withdrawfromvault("5cbe99407674a689fa9b8a522462b7a4b3e7893f61453ce3fa77f1307f7d0600", "df1qw8c57c3c4u7k2h4gv2d5x4jr4qgq6cugg33g6e", "100@DFI") # withdraws collateral from a vault
 
         :param vaultId: (required) vault id
         :type vaultId: str
-        :param addressTo: (required) destination address for withdraw of collateral
+        :param addressTo: (required) destination address for withdrawn collateral
         :type addressTo: str
-        :param amount: (required) Amount of collateral in amount@symbol format
-        :type amount: str
-        :param inputs: (optional) Inputs
-        :type inputs: TxInput
+        :param amount: (required) string with amount and token separated by an @
+        :type amount: :ref:`Transactions Amounts`
+        :param inputs: (optional) additional inputs to spend
+        :type inputs: [TxInput]
         :return: Transaction
         """
 
